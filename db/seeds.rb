dbce4784d2f1207123a74de6df17825610759f35
ben <benjamin.n.chapin@gmail.com>
2020-02-20 20:20:48 -0600

    adds example-places-app

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+Place.create(name: "Disney World", address: "2364 Plainfield Rd, Crest Hill, IL 60403")
+Place.create(name: "SeaWorld", address: "7007 Sea World Dr, Orlando, FL 32821")
+Place.create(name: "Shed Aquarium", address: "1200 S Lake Shore Dr, Chicago, IL 60605")
+Place.create(name: "Field Museum", address: "1400 S Lake Shore Dr, Chicago, IL 60605")
